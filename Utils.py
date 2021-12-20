b7690a4825a5b1bab159dd6e07f0ea991b7e79c1
Ricardo <ricardo.rg462@gmail.com>
2021-12-20 18:18:53 -0300

    Fixing peak to peak voltage in linear_chirp

--- Utils.py
+++ Utils.py
@@ -3,5 +3,5 @@ import numpy as np
 def linear_chirp(peak_to_peak, T, initial_frecuency, final_frecuency, f_s = 48e3, initial_phase=0):
     c = (final_frecuency - initial_frecuency) / T
     t = np.linspace(0, T, f_s*T)
-    x = peak_to_peak * np.sin(initial_phase + 2*np.pi*(c*t**2/2 + initial_frecuency * t))
+    x = peak_to_peak/2 * np.sin(initial_phase + 2*np.pi*(c*t**2/2 + initial_frecuency * t))
     return t,x
